72bdffb126c0483fca030de68e4b13d9827eb16c
Yogesh Singh <yogesh.singh@hbwsl.com>
2023-10-30 15:06:19 +0530

    created test class and injected the CategoryInterface as a dependency in the constructor along with two other constructor arguments and created a method to display it

---
+++ app/code/Yogesh/Test.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Yogesh;
+
+use Magento\Catalog\Api\Data\CategoryInterface;
+
+class Test
+{
+    protected $category;
+    protected $array;
+    protected $string;
+
+    public function __construct(CategoryInterface $category, array $array = [1, 2, 2], string $string = "Hi!!")
+    {
+        $this->category = $category;
+        $this->array = $array;
+        $this->string = $string;
+    }
+
+
+    public function displayParams()
+    {
+
+        echo 'Array: ' . print_r($this->array, true);
+        echo 'String: ' . $this->string;
+    }
+}
