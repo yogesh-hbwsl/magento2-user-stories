e34611a9fd59774b155fd6727fc8002f1730995a
Yogesh Singh <yogesh.singh@hbwsl.com>
2023-10-31 12:11:46 +0530

    Create a template with the text and display on all pages

---
+++ app/code/Yogesh/Mod1/Block/ShowOnAll.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Yogesh\Mod1\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\View\Element\Template\Context;
+
+class ShowOnAll extends Template
+{
+    public function __construct(Context $context)
+    {
+        parent::__construct($context);
+    }
+}
