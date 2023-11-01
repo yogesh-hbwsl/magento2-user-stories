d4d764bd35854ecdafa56586d1433a515f11cf1b
Yogesh Singh <yogesh.singh@hbwsl.com>
2023-11-01 11:36:54 +0530

    created backend controller which shows config value

--- app/code/Yogesh/Mod1/Controller/Adminhtml/ShowText/Index.php
+++ app/code/Yogesh/Mod1/Controller/Adminhtml/ShowText/Index.php
@@ -3,12 +3,31 @@
 namespace Yogesh\Mod1\Controller\Adminhtml\ShowText;
 
 use Magento\Framework\App\ActionInterface;
+use \Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\ScopeInterface;
 
 class Index implements ActionInterface
 {
+    public $scopeConfig;
+
+    public function __construct(ScopeConfigInterface $scopeConfig)
+    {
+        $this->scopeConfig = $scopeConfig;
+    }
+
     public function execute()
     {
-        echo "show";
+        $isEnabled =  $this->scopeConfig->getValue('general/parameters/enable', ScopeInterface::SCOPE_STORE);
+        if ($isEnabled) {
+            $text = $this->scopeConfig->getValue('general/parameters/display_text', ScopeInterface::SCOPE_STORE);
+            if (empty($text)) {
+                echo "Text field is empty";
+            } else {
+                echo $text;
+            }
+        } else {
+            echo "Not enabled";
+        }
         exit;
     }
 }
